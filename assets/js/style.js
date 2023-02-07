dcd9123c23dd67d46ae1731885f0560d55a5ab18
AsadAli-1122 <asadali.cool.23@gmail.com>
2023-02-07 18:00:25 +0500

    nn

---
+++ assets/js/style.js
@@ -0,0 +1,11 @@
+
+function clearRadioButtons(){
+    var ele = document.querySelectorAll("input[type=radio]");
+    for(var i=0;i<ele.length;i++){
+        ele[i].checked = false;
+    }
+    }
+
+
+
+    
