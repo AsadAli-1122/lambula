b6f944a0197376dde4d9795c04811b01272c297c
AsadAli-1122 <asadali.cool.23@gmail.com>
2023-02-08 03:01:13 +0500

    nn

--- assets/js/style.js
+++ assets/js/style.js
@@ -4,13 +4,28 @@ function clearRadioButtons(){
     for(var i=0;i<ele.length;i++){
         ele[i].checked = false;
     }
-    }
+}
 
 
 function readMore() {
     var dots = document.getElementById("dots1");
     var moreText = document.getElementById("more1");
     var btnText = document.getElementById("myBtn1");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more <i class='fa-solid fa-caret-down'></i>";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less <i class='fa-solid fa-caret-up'></i>";
+        moreText.style.display = "inline";
+    }
+}
+function readMore2() {
+    var dots = document.getElementById("dots2");
+    var moreText = document.getElementById("more2");
+    var btnText = document.getElementById("myBtn2");
     
     if (dots.style.display === "none") {
         dots.style.display = "inline";
@@ -22,21 +37,9 @@ function readMore() {
         moreText.style.display = "inline";
     }
     }
-    function readMore2() {
-        var dots = document.getElementById("dots2");
-        var moreText = document.getElementById("more2");
-        var btnText = document.getElementById("myBtn2");
-        
-        if (dots.style.display === "none") {
-            dots.style.display = "inline";
-            btnText.innerHTML = "Read more <i class='fa-solid fa-caret-down'></i>";
-            moreText.style.display = "none";
-        } else {
-            dots.style.display = "none";
-            btnText.innerHTML = "Read less <i class='fa-solid fa-caret-up'></i>";
-            moreText.style.display = "inline";
-        }
-        }
-    
 
-        
+
+setInterval(function load(){
+        var x = document.getElementById('loader');
+        x.classList.add('hidden');
+    }, 2000)
